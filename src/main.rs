27d96db8ea6dedef06138b71aa47970c2f1dfaf7
wejick <wejick@gmail.com>
2017-03-02 20:45:52 +0700

    tmp

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn searchAdress(keyword: &str) {
 }
 
 fn main() {
-    let mut client = make_client("http://localhost:9200");
+    let mut client = make_client("http://192.168.100.16:9200");
 
     let result_wrapped : Result<search::SearchResult<example_service_search::AddressDocument>,error::EsError> = client.search_query()
                    .with_indexes(&["address"])
@@ -34,12 +34,21 @@ fn main() {
 
     let mut router = Router::new();
 
-    let ok = |_: &mut Request| -> IronResult<Response> {
-        let doc = example_service_search::AddressDocument::new().with_name("Gio");
-        let response = serde_json::to_string(&doc).unwrap();
-        Ok(Response::with((iron::status::Ok, response.as_str())))
+    let ok = |req: &mut Request| -> IronResult<Response> {
+        // let ref name = req.extensions.get::<Router>().unwrap().find("name").unwrap_or("/");
+        
+        let result_wrapped : Result<search::SearchResult<example_service_search::AddressDocument>,error::EsError> = client.search_query()
+                   .with_indexes(&["address"])
+                   .with_query(&Query::build_match("name", "").build())
+                   .send();
+        
+        let result = result_wrapped.unwrap();
+        let search_result = result.hits.hits;
+        println!("TEST RESULT: {:?}", result_wrapped);
+        //let response = serde_json::to_string(&search_result).unwrap();
+        Ok(Response::with((iron::status::Ok, "")))
     };
 
-    router.get("/", ok, "ok");
+    router.get("/:name", ok, "ok");
     Iron::new(router).http("localhost:3000").unwrap();
 }
